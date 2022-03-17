e44ac4c2b617a61f5911e0e3e62adb373e8a1b15
Shafa Amira <shafaqonitatin@gmail.com>
2022-03-17 16:39:25 +0700

    add close button

--- 04-modal-window/script.js
+++ 04-modal-window/script.js
@@ -8,7 +8,16 @@ console.log(btnsShowModal)
 
 for(let i=0; i<btnsShowModal.length; i++)
     btnsShowModal[i].addEventListener('click', function () {
-        console.log('Button Clicked!');
+        console.log('Button Open Clicked!');
         // remove the hidden in our modal class
-        modal.classList.remove('hidden')
+        modal.classList.remove('hidden') 
     })
+    
+
+btnCloseModal.addEventListener('click', function () {
+    console.log('Button Close Clicked!');
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+})
+
+      
