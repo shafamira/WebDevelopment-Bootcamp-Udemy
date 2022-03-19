f348e1d5228d498289c905c2d147baca150126a2
Shafa Amira <shafaqonitatin@gmail.com>
2022-03-19 22:06:25 +0700

    modal-window donee

--- 04-modal-window/script.js
+++ 04-modal-window/script.js
@@ -6,18 +6,39 @@ const btnCloseModal = document.querySelector('.close-modal')
 const btnsShowModal = document.querySelectorAll('.show-modal')
 console.log(btnsShowModal)
 
-for(let i=0; i<btnsShowModal.length; i++)
-    btnsShowModal[i].addEventListener('click', function () {
-        console.log('Button Open Clicked!');
-        // remove the hidden in our modal class
-        modal.classList.remove('hidden') 
-    })
+// for(let i=0; i<btnsShowModal.length; i++)
+//     btnsShowModal[i].addEventListener('click', function () {
+//         console.log('Button Open Clicked!');
+//         // remove the hidden in our modal class
+//         modal.classList.remove('hidden') 
+//     })
     
 
-btnCloseModal.addEventListener('click', function () {
-    console.log('Button Close Clicked!');
+// btnCloseModal.addEventListener('click', function () {
+//     console.log('Button Close Clicked!');
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden');
+// })
+
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+  
+const closeModal = function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
-})
+};
 
-      
+for (let i = 0; i < btnsShowModal.length; i++)
+  btnsShowModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
